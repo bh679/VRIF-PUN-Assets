6a87ebb5d195565fd1b7025fcdccc1bfe9332188
Brennan Hatton MSI <bernnan@equalreality.com>
2023-02-20 15:32:17 +1100

    Custom emulator controlls

---
+++ Runtime/Scripts/PhotonChangeServer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Photon.Pun;
+
+namespace BrennanHatton.Networking
+{
+
+	public class PhotonChangeServer : MonoBehaviourPunCallbacks
+	{
+		static bool isDefault = true;
+		public string defaultRegion;
+		public string target = "au";
+		public UnityEvent onChange;
+		public NetworkManager netManager;
+		bool defaultAutoReconnect;
+		
+		void Start()
+		{
+			defaultAutoReconnect = netManager.autoReconnect;
+			SetDefault();
+		}
+		
+		void SetDefault()
+		{
+			
+			defaultRegion = PhotonNetwork.CloudRegion;
+			
+			if(defaultRegion == target)
+				target = "usw";
+		}
+		
+		public void ChangeToTarget(string targ = null)
+		{
+			if(!string.IsNullOrEmpty(targ))
+				target = targ;
+				
+			netManager.autoReconnect = false;
+			PhotonNetwork.Disconnect();
+			PhotonNetwork.ConnectToRegion(target);
+			onChange.Invoke();
+		}
+		
+		public void ChangeToDefault()
+		{
+			netManager.autoReconnect = true;
+			netManager.reconnectDelay = 0f;
+			PhotonNetwork.Disconnect();
+			onChange.Invoke();
+		}
+		
+		public void SwitchTargetDefault()
+		{
+			if(isDefault)
+				ChangeToTarget();
+			else
+				ChangeToDefault();
+				
+			isDefault = !isDefault;
+		}
+		
+		
+		public override void OnJoinedRoom()
+		{
+			if(string.IsNullOrEmpty(defaultRegion))
+			{
+				SetDefault();
+			}
+			
+			netManager.autoReconnect = defaultAutoReconnect;
+			base.OnJoinedRoom();
+		}
+		
+	}
+
+}
