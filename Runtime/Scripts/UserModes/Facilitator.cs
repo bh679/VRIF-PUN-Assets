9500b70eebc43a5a651d17567eedd9622a653d60
Brennan Hatton <brennan@brennanhatton.com>
2023-04-07 20:24:15 +0800

    URP shaders for Avatars

--- Runtime/Scripts/UserModes/Facilitator.cs
+++ Runtime/Scripts/UserModes/Facilitator.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
 
 namespace EqualReality.MetaverseClassrooms
 {
 
-	public class Facilitator : MonoBehaviour
+	public class Facilitator : MonoBehaviour//, IOnEventCallback
 	{
 		public static bool mode = false;
+		/*public static List<int> otherFaciltiators = new List<int>();
 		
+		private void OnEnable()
+		{
+			PhotonNetwork.AddCallbackTarget(this);						
+		}
+	
+		private void OnDisable()
+		{
+			PhotonNetwork.RemoveCallbackTarget(this);
+		}
+		
+		public void OnEvent(EventData photonEvent)
+		{
+			byte eventCode = photonEvent.Code;
+			
+			if(eventCode == ClassroomSendEventManager.BecomeFacilitatorEventCode)
+			{
+				object[] data = (object[])photonEvent.CustomData;
+				int id = (int)data[0];
+				bool isFacilitator = (bool)data[1];
+				
+				if(id != PhotonNetwork.LocalPlayer.ActorNumber)
+				{
+					if(isFacilitator)
+						otherFaciltiators.Add(id);
+					else 
+					{
+						while(otherFaciltiators.Contains(id))
+							otherFaciltiators.Remove(id);
+					}
+				}
+			}
+		}
 		
 		public void SetFacilitatorFromToggle(Toggle toggle)
 		{
@@ -31,7 +67,7 @@ namespace EqualReality.MetaverseClassrooms
 		public void SetMode(bool newModeStatus)
 		{
 			mode = newModeStatus;
-		}
+		}*/
 	}
 
 }
